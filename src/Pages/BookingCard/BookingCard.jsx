43c215adafd0c2be1b6abcedb3312e59ff2e7fe2
Abu-Sahad <abusahad603@gmail.com>
2023-06-04 21:24:54 +0600

    booking page data set tabular format and pending status upadate

--- src/Pages/BookingCard/BookingCard.jsx
+++ src/Pages/BookingCard/BookingCard.jsx
@@ -1,34 +1,34 @@
-const BookingCard = ({ booking }) => {
-    const { _id, message, img, date, price } = booking
-    const handleDelete = (_id) => {
-        console.log('deleted', _id)
-    }
+const BookingCard = ({ booking, handleDelete, handleUpdate }) => {
+    const { _id, img, date, price, status } = booking;
+
+
+
     return (
         <tr>
             <th>
                 <label>
                     <button onClick={() => handleDelete(_id)} className="btn btn-square bg-[black] text-white btn-outline">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
                     </button>
                 </label>
             </th>
             <td>
                 <div className="avatar">
-                    {img && <div className="mask mask-squircle w-24 h-24">
-                        <img src={img} />
-                    </div>}
+                    {img && (
+                        <div className="mask mask-squircle w-24 h-24">
+                            <img src={img} />
+                        </div>
+                    )}
                 </div>
-
             </td>
-            <td>
-                {price}
-
-            </td>
+            <td>{price}</td>
             <td>{date}</td>
             <th>
-                <button className="btn btn-ghost bg-[#FF3811] text-white btn-xs">Pending</button>
+                {status === 'confirm' ? <span className="btn btn-ghost bg-[#FF3811] text-white btn-xs">Order Approve</span> : < button onClick={() => handleUpdate(_id)} className="btn btn-ghost bg-[#FF3811] text-white btn-xs">Pending</button>}
             </th>
-        </tr>
+        </tr >
     );
 };
 
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import BookingCard from "../BookingCard/BookingCard";
 import { key } from "localforage";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
@@ -20,6 +21,76 @@ const Bookings = () => {
                 });
         }
     }, [user]);
+    const handleDelete = (id) => {
+        console.log(id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/booking/${id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(res => {
+                            if (res.status === 200) {
+                                return res.json();
+                            } else {
+                                throw new Error('Failed to delete the booking');
+                            }
+                        })
+                        .then(data => {
+                            console.log(data);
+                            if (data.deletedCount > 0) {
+                                Swal.fire(
+                                    'Deleted!',
+                                    'Your coffee has been deleted.',
+                                    'success'
+                                );
+                                const remaining = bookings.filter(booking => booking._id !== id)
+                                setBookings(remaining)
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error);
+                            Swal.fire(
+                                'Error',
+                                'Failed to delete the booking',
+                                'error'
+                            );
+                        });
+                }
+            });
+    };
+
+    const handleUpdate = id => {
+        fetch(`http://localhost:5000/booking/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    const remaining = bookings.filter(booking => booking._id !== id)
+                    const updated = bookings.find(booking => booking._id === id)
+                    updated.status = 'confirm'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings)
+                }
+            })
+    }
+
+
+
 
     return (
         <div>
@@ -46,6 +117,8 @@ const Bookings = () => {
                                 bookings.map(booking => < BookingCard
                                     key={booking._id}
                                     booking={booking}
+                                    handleDelete={handleDelete}
+                                    handleUpdate={handleUpdate}
                                 ></BookingCard>)
                             }
 
